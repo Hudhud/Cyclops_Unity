dd2ed9ba247971eb24f7239d35f89c13a5521fbb
Hadi <hadihorani@hotmail.com>
2016-10-24 22:15:31 +0200

    Particle effect tilføjet til dåsen der skydes
    Particle effect tilføjet til dåsen der skydes, men der skal fjernes
    noget: når man kører spillet så starter en effect af sig selv og
    forsvinder

--- Assets/Script/Hit.cs
+++ Assets/Script/Hit.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Hit : MonoBehaviour {
 
+    public GameObject effect;
+    private ParticleSystem _psystem;
+
     // Use this for initialization
     void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        effect = GameObject.Find("HitEffect");
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
     public void Disappear()
     {
         Destroy(gameObject);
+        Instantiate(effect, transform.position, transform.rotation);
     }
-
-        }
+}
