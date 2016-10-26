aed3a2eb17d529c873d140a451cddbb433c73e86
Hadi <hadihorani@hotmail.com>
2016-10-26 21:19:23 +0200

    Countdown er implementeret
    Tæller fra 10 til 0. Der mangles at håndtere hvad der skal ske, når den
    har nået 0. Koden er gjort klar

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountDown : MonoBehaviour {
+
+    float timeLeft = 10.0f;
+    public TextMesh countdown;
+
+    // Use this for initialization
+    void Start () {
+        countdown = gameObject.GetComponent<TextMesh>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (timeLeft >= 0)
+        {
+            timeLeft -= Time.deltaTime;
+            countdown.text = "Time Left:" + Mathf.Round(timeLeft);
+        }
+            
+        //Her skal der implementeres hvad der skal gøres, når tiden er gået
+        if (timeLeft <= 0)
+        {
+            Debug.Log("Hej");
+        }
+    }
+}
