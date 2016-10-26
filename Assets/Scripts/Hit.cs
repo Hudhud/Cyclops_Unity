c6b33f84a61079fb31c60f687b9a9360e104c867
Hadi <hadihorani@hotmail.com>
2016-10-26 12:48:18 +0200

    Counter til dåserne er implementeret
    Counter til dåserne er implementeret, så tælles der for hver gang man
    skyder en dåse. Det bliver skrevet på tavlen

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -5,20 +5,27 @@ using System;
 public class Hit : MonoBehaviour {
 
     public GameObject effect;
-    private ParticleSystem _psystem;
+    private static int count = 0;
+    public TextMesh countText;
 
     // Use this for initialization
     void Start () {
         effect = GameObject.Find("HitEffect");
+        countText = GameObject.Find("CountText").GetComponent<TextMesh>();
+        countText.text = "Count: " + count.ToString();
     }
 
     // Update is called once per frame
     void Update () {
-	
 	}
+
     public void Disappear()
     {
         Destroy(gameObject);
+        count++;
+        countText.text = "Count: " + count.ToString();
         Instantiate(effect, transform.position, transform.rotation);
     }
+
+    
 }
