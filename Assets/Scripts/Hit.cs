65a3a19aabd9a2eb615ed19379ecfb995f55c9f0
Emdadollah <emdah1@hotmail.com>
2016-10-31 16:54:09 +0100

    Eksplosion på dåser

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -1,19 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using System.Threading;
 
 public class Hit : MonoBehaviour {
 
     public GameObject effect;
     private static int count = 0;
     public TextMesh countText;
-
+    public Texture2D textureToUse;
     public AudioClip destroySound;
     private AudioSource source;
 
     // Use this for initialization
     void Start () {
-        effect = GameObject.Find("HitEffect");
+        effect = GameObject.Find("Explosion");
         countText = GameObject.Find("CountText").GetComponent<TextMesh>();
         countText.text = "Count: " + count.ToString();
         source = GetComponent<AudioSource>();
@@ -25,13 +26,14 @@ public class Hit : MonoBehaviour {
 
     public void Disappear()
     {
+        
         source.PlayOneShot(destroySound, 1F);
-        GetComponent<Renderer>().enabled = false;
-        Destroy(gameObject, destroySound.length);
+        GetComponent<Renderer>().enabled = false;        
         count++;
         countText.text = "Count: " + count.ToString();
         Instantiate(effect, transform.position, transform.rotation);
     }
 
-    
+
+
 }
