d0ba940227081a7073de8d64625878122f758331
Olivier <w.n.w.a.up@gmail.com>
2016-11-14 02:25:14 +0100

    level 4
    mangler at skyde dåserne fordi ???

---
+++ Assets/Scripts/MoveToCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToCamera : MonoBehaviour
+{
+
+    public float speed;
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	    if (transform.position.Equals(target.position))
+	    {
+	        print("it's lit");
+	    }
+    }
+}
