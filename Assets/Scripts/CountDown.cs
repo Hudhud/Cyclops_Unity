a00119241890c3f22f0307e766148a546ad25cd6
Olivier <w.n.w.a.up@gmail.com>
2016-11-14 15:12:20 +0100

    level text
    3D text, kan ikke stoppe spillet

--- Assets/Scripts/CountDown.cs
+++ Assets/Scripts/CountDown.cs
@@ -5,32 +5,44 @@ using UnityEngine.SceneManagement;
 public class CountDown : MonoBehaviour {
 
     float timeLeft = 30.0f;
-    public TextMesh countdown;
+    public TextMesh countdown, gameOverText;
     static int i;
+    public Hit counter;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
+        gameOverText.gameObject.SetActive(false);
+        counter = gameObject.AddComponent<Hit>();
         countdown = GameObject.Find("CountDownText").GetComponent<TextMesh>();
         i = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
     void Update () {
-       
+
         if (timeLeft >= 0)
         {
             timeLeft -= Time.deltaTime;
             countdown.text = "Time: " + Mathf.Round(timeLeft);
-        }
-            
+
             if (GameObject.FindGameObjectWithTag("Can") == null)
             {
-            Hit counter = new Hit();
-            counter.Restartcounter();
-            SceneManager.LoadScene(++i);
+                gameOverText.gameObject.SetActive(true);
+                counter.Restartcounter();
+                StartCoroutine(Wait(3000));
+                SceneManager.LoadScene(++i);
             }
-            if (timeLeft <= 0)
-            {
+
         }
+        else
+        {
+
+        }
+    }
+
+    IEnumerator Wait(float sec)
+    {
+        yield return new WaitForSeconds(sec);
     }
 }
