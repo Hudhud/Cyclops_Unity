9f2efb8138286665f9d7f676cd73617061cdd0fb
Hadi <hadihorani@hotmail.com>
2016-11-28 15:27:47 +0100

    Wins/losses tekst justeres i forhold til mobil skærm

---
+++ Assets/Scripts/MobileScreenAdjustment.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.VR;
+
+public class MobileScreenAdjustment : MonoBehaviour
+{
+    public Text WinText, LossText;
+	// Use this for initialization
+    void Start()
+    {
+        float width = Screen.width;
+        float height = Screen.height;
+        Vector3 winposition = WinText.transform.position;
+        winposition.x = (width*1/5);
+        winposition.y = (height*1/5);
+        winposition.z = -185.9f;
+        Vector3 lossposition = LossText.transform.position;
+        lossposition.x = (width * 1/5);
+        lossposition.y = (height * 1/5)-LossText.preferredHeight;
+        lossposition.z = -185.9f;
+
+
+        WinText.transform.position = winposition;
+        LossText.transform.position = lossposition;
+    }
+
+}
